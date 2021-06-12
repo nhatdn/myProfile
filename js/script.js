b5674cc8a0c404b5eaeed474dd37d6711806fb45
nhatdn <nhatdn.it@gmail.com>
2021-06-12 22:10:30 +0700

    update mesage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-function $(name) {
-    return document.querySelector("." +name);
-}
-
-console.log($("container-body-content"));
+var messageBtn = document.querySelector(".messenger a");
+var listMessage = document.querySelector(".show-list-my-message");
+var iconMess = document.querySelector(".fab.fa-facebook-messenger");
+messageBtn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    if(listMessage.classList.contains("showMessage")) {
+        listMessage.classList.remove("showMessage");
+    } else {
+        listMessage.classList.add("showMessage");
+    }
+});
+document.addEventListener("click",(e)=>{
+    if((e.target != messageBtn) && (e.target !=iconMess)) {
+            listMessage.classList.remove("showMessage");  
+    }
+})
